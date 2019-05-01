123bf83af79f8b061437360910c21b62b07b6da5
Maks <mmasyush@gmail.com>
2019-05-01 19:47:38 +0300

    Created Doom E1M1 map with ProBuider and movable player

---
+++ Assets/Jump.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    public float jvel;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("SDpace is preset");
+            GetComponent<Rigidbody>().velocity = Vector3.up * jvel;
+        }
+    }
+}
---
+++ Assets/Moves.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moves : MonoBehaviour
+{
+    public float speed = 3f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        var move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        var rot = new Vector3(0, Input.GetAxis("Mouse X"), 0);
+
+        transform.Translate(move * speed * Time.deltaTime);
+        transform.Rotate(rot);
+    }
+}
